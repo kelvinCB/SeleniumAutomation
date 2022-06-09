2ca8d839833692db7d716bf4ee0367070ffd5527
Kelvin Calcano <kelvinr02@hotmail.com>
2022-06-09 12:13:29 -0400

    Optimizing libraries imported

--- src/test_cases/GetDataFromTableStream2.java
+++ src/test_cases/GetDataFromTableStream2.java
@@ -1,16 +1,12 @@
 package test_cases;
 
-import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.Iterator;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 
 import constants.ObjectConstants;
