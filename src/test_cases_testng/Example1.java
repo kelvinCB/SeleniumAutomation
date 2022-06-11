2d8282b86e7526383cb38d80767332f5fd621cf0
kelvinCB <kelvinr02@hotmail.com>
2022-06-10 23:14:56 -0400

    Adding TestNG framework to my project, Running TestNG test cases, Running Test Suites

---
+++ src/test_cases_testng/Example1.java
@@ -0,0 +1,18 @@
+package test_cases_testng;
+
+import org.testng.annotations.Test;
+
+public class Example1 {
+
+	@Test
+	public void test1() {
+		System.out.println("This is Test1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("This is Test2");
+	}
+	
+	
+}
---
+++ src/test_cases_testng/Example2.java
@@ -0,0 +1,17 @@
+package test_cases_testng;
+
+import org.testng.annotations.Test;
+
+public class Example2 {
+	
+	@Test
+	public void test3() {
+		System.out.println("This is Test3");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("This is Test4");
+	}
+
+}
