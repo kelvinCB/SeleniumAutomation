0c8bb9aabb62740f236f7f2836b792db67682162
Kelvin Calcano <kelvinr02@hotmail.com>
2022-06-27 03:55:11 -0400

    Using Log4j to log errors, fatals, warnings, info, and debugs

--- src/test_cases/Log4jDemo.java
+++ src/test_cases/Log4jDemo.java
@@ -5,16 +5,29 @@ import org.apache.logging.log4j.Logger;
 
 public class Log4jDemo {
 
-	private static Logger log =LogManager.getLogger(Log4jDemo.class.getName());
+	/*
+	 * Use log. Error()
+	 * to log when elements are not displayed in the page or if anyvalidations fail
+	 * 
+	 * Use Log. Debug()
+	 * When each Selenium action is performed like click, SendKeys, getText()
+	 * 
+	 * Use log.info()
+	 * When operation is successfully completed ex: After loading page, or after any
+	 * successful validations
+	 * It’s just counterpart to log. Error()
+	 * 
+	 */
+	private static Logger log = LogManager.getLogger(Log4jDemo.class.getName());
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		log.info("This is info message");
 		log.debug("This is debug message");
 		log.error("This is error message");
 		log.warn("This is warn message");
 		log.fatal("This is fatal message");
-		
 
 	}
 
